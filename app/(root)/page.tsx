759d7cc7cfc1c69426e5485ae6b2be10198a2c6d
Faheem Mansuri <mansurifaheem1111@google.com>
2025-12-27 19:53:06 +0530

    Save Changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
 import { Button } from "@/components/ui/button"
+import TradingViewWidget from "@/components/TradingViewWidget";
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen home-wrapper">
-      <Button>Click Me!</Button>
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget 
+            title="Market Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className="custome-chart"
+            height={600}
+          />  
+        </div>
+        <div className="">
+
+        </div>
+      </section>
     </div>
   )
 }
