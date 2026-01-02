cba4881ac069c89b9de100b54a3134264c4ab32c
Faheem Mansuri <mansurifaheem1111@google.com>
2026-01-02 18:35:50 +0530

    MongoDB Connection

---
+++ database/mongoose.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    }
+}
+
+let cached = global.mongooseCache;
+
+if(!cached) {
+    cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+    if(!MONGODB_URI) throw new Error('MONGODB_URI must be set within .env');
+
+    if(cached.conn) return cached.conn;
+
+    if(!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (err) {
+        cached.promise = null;
+        throw err;
+    }
+
+    const dbName = mongoose.connection?.name || '(unknown)';
+    const host = mongoose.connection?.host || '(unknown)';
+    console.log(`Connected to database [db="${dbName}", host="${host}", env="${process.env.NODE_ENV || 'development'}"]`);
+
+    return cached.conn;
+}
